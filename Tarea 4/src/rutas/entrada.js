ab2a5a5f0b249d6cf558799070477696d2b90a13
valrmzl <valeria.ramirez@iteso.mx>
2023-03-13 15:54:36 -0600

    Tarea 5 Val

--- Tarea 4/src/rutas/entrada.js
+++
@@ -1,9 +0,0 @@
-
-const express= require('express');
-const router=express.Router();
-
-router.get('', function(req,res){
-    res.render('pagina1');
-})
-//antes de definir las rutas hay que exportar el middleware
-module.exports= router;
--- Tarea 4/src/rutas/index.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router=express.Router();
-
-const rutaNoticias= require('./noticias');
-const rutaEntrada=require('./entrada');
-router.use('/', rutaEntrada);
-router.use('/noticias',rutaNoticias);
-module.exports=router;
